ef8f69c103081a6597d37e9a28a3e491e9236177
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2022-09-24 14:23:58 -0600

    Black background, launcher movement

---
+++ A1-CS 408/Assets/MoveLauncher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLauncher : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0f, 0.1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0.0f, -0.1f, 0f);
+        }
+    }
+}
